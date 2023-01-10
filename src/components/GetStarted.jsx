b5a3a084b3e38dd72fc38edb6e8a87d6a0216451
unknown <dubeyprakhar13@gmail.com>
2023-01-10 13:22:20 +0530

    business section added

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -12,13 +12,23 @@ const GetStarted = () => {
      <div className={`${styles.flexCenter} flex-col
       bg-primary w-[100%] h-[100%] rounded-full`}>
 <div className={`${styles.flexStart} flex-row`}>
-<p className='font-poppins font-medium text-[18px] leading-[23px]'> 
+<p className='font-poppins font-medium 
+text-[18px] leading-[23px] mr-2'> 
   <span className='text-gradient'>
     GET
   </span>
+ 
 </p>
-
+ <img src={arrowUp} alt="arrow" 
+ className="w-[23px] h-[23px] object-contain"/>
 </div>
+
+<p className='font-poppins font-medium text-[18px] leading-[23px]'> 
+  <span className='text-gradient'>
+   STARTED
+  </span>
+  
+</p>
      </div>
     </div>
   )
--- src/components/index.js
+++ src/components/index.js
@@ -9,6 +9,7 @@ import Footer from "./Footer";
 import Testimonials from "./Testimonials";
 import Hero from "./Hero";
 
+
 export {
   NavBar,
   Billing,
@@ -20,4 +21,5 @@ export {
   Footer,
   Testimonials,
   Hero,
+  
 };
